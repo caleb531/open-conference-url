c84a8e117350e4ff271253846fd964ed46ad3809
Caleb Evans <caleb@calebevans.me>
2020-11-08 17:49:29 -0800

    Remove date_format and time_format from preferences JSON
    They are meant for internal use and should not be modified.

--- prefs.py
+++ prefs.py
@@ -6,6 +6,11 @@ import json
 # The user's preferences for this workflow
 class Prefs(object):
 
+    # The date and time used internally to display and parse icalBuddy event
+    # output; ***do not change this***
+    date_format = '%Y-%m-%d'
+    time_format = '%H:%M'
+
     def __init__(self):
         with open('prefs.json', 'r') as prefs_json:
             # Make all JSON keys accessible as instance attributes
