3d016d3ae194bca17a2f60fc49f6c2f033caf015
Caleb Evans <caleb@calebevans.me>
2022-08-10 12:58:13 -0700

    Clarify that calendar events are for today

--- ocu/list_events.py
+++ ocu/list_events.py
@@ -97,14 +97,14 @@ def main():
     if not all_events and not upcoming_events:
         feedback['items'].append({
             'title': 'No Results',
-            'subtitle': 'No calendar events could be found',
+            'subtitle': 'No calendar events for today',
             'valid': 'no'
         })
     elif all_events and not upcoming_events:
         upcoming_events = all_events
         feedback['items'].append({
             'title': 'No Results',
-            'subtitle': 'Showing all calendar events',
+            'subtitle': 'Showing all calendar events for today',
             'valid': 'no'
         })
     feedback['items'].extend(get_event_feedback_item(event)
