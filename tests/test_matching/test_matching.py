1ec7289e8f4fc5c5bef01b0ccc40a44e0b486dce
Caleb Evans <caleb@calebevans.me>
2022-11-28 12:26:03 -0800

    Combine tests into single file
    Also consolidate the redundant file-reading logic.

---
+++ tests/test_matching/test_matching.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import os.path
+import unittest
+
+from ocu.event import Event
+
+
+EVENT_DATA_DIR = os.path.join('tests', 'sample_events')
+
+
+tc = unittest.TestCase()
+
+
+def get_event_from_file(file_name):
+    with open(os.path.join(EVENT_DATA_DIR, file_name), 'r') as json_file:
+        raw_data = json.load(json_file)
+        return Event(raw_data)
+
+
+def test_zoom():
+    event = get_event_from_file('zoom.json')
+    tc.assertEqual(
+        event.conference_url,
+        'https://zoom.us/l/123456?usp=sharing')  # noqa
+
+
+def test_teams():
+    event = get_event_from_file('teams.json')
+    tc.assertEqual(
+        event.conference_url,
+        'https://teams.microsoft.com/l/meetup-join/12%3ameeting_A1b2C3%40thread.v2/0?context=%7b%22Tid')  # noqa
