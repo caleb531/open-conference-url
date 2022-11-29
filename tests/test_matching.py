6c16c1c9ca2604eb9466eed708b99aa273d6a3c8
Caleb Evans <caleb@calebevans.me>
2022-11-28 17:20:14 -0800

    Test many more permutations of cases through test generation

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import itertools
 import json
 import os
 import os.path
@@ -12,51 +13,87 @@ from tests.utilities import use_env
 EVENT_DATA_DIR = os.path.join('tests', 'sample_events')
 
 
-tc = unittest.TestCase()
+case = unittest.TestCase()
+
+
+def get_test_data():
+    """
+    Retrieve a dictionary of information pertaining to the various test cases to
+    test manually or generate automatically
+    """
+    test_data_path = os.path.join('tests', 'test_data.json')
+    with open(test_data_path, 'r') as test_data_file:
+        return json.load(test_data_file)
 
 
 def get_event_from_file(file_name):
+    """Parse a JSON object from the specified file into an Event object"""
     with open(os.path.join(EVENT_DATA_DIR, file_name), 'r') as json_file:
         raw_data = json.load(json_file)
         return Event(raw_data)
 
 
-def test_zoom():
-    event = get_event_from_file('zoom.json')
-    tc.assertEqual(
-        event.conference_url,
-        'https://zoom.us/j/123456?pwd=AiBjCk')
+def get_event_with_defaults(**kwargs):
+    """
+    Build a new Event object from the given service dictionary, with any
+    additional parameters
+    """
+    return Event({
+        'title': kwargs.get('title', 'Meeting'),
+        'startDate': kwargs.get('startDate', '2011-10-16T08:00'),
+        'endDate': kwargs.get('endDate', '2011-10-16T09:00'),
+        **kwargs
+    })
+
+
+def generate_location_test_cases(service):
+    """Should parse conference URLs in the 'Location' field of the event"""
+    for correct_url in service['example_correct_urls']:
+        event = get_event_with_defaults(
+            title='{} Call'.format(service["name"]),
+            location=correct_url)
+        yield case.assertEqual, event.conference_url, correct_url
+
+
+def generate_notes_test_cases(service):
+    """
+    Should parse all permutations of correct conference URL in Notes field
+    containing other URLs which should be ignored
+    """
+    # TODO
+    for correct_url in service['example_correct_urls']:
+        base_permutation = service['example_incorrect_urls'] + [correct_url]
+        permutations = itertools.permutations(base_permutation)
+        for permutation in permutations:
+            for start_token, end_token in ('""', '\'\'', '<>'):  # TODO
+                wrapped_urls = [(start_token + url + end_token)
+                                for url in permutation]
+                event = get_event_with_defaults(
+                    title='{} Call'.format(service["name"]),
+                    notes=' '.join(wrapped_urls)
+                )
+                yield case.assertEqual, event.conference_url, correct_url
+
+
+def test_permutations():
+    """
+    Test all permutations of test cases that the workflow should correctly
+    handle
+    """
+    test_data = get_test_data()
+    for service in test_data['services']:
+        yield from generate_location_test_cases(service)
+        yield from generate_notes_test_cases(service)
 
 
 @use_env('use_direct_zoom', 'true')
 def test_zoom_direct():
     event = get_event_from_file('zoom.json')
-    tc.assertEqual(
+    case.assertEqual(
         event.conference_url,
         'zoommtg://zoom.us/join?action=join&confno=123456&pwd=AiBjCk')
 
 
-def test_google_meet():
-    event = get_event_from_file('google-meet.json')
-    tc.assertEqual(
-        event.conference_url,
-        'https://meet.google.com/abc-defg-hij')
-
-
-def test_microsoft_teams():
-    event = get_event_from_file('microsoft-teams.json')
-    tc.assertEqual(
-        event.conference_url,
-        'https://teams.microsoft.com/l/meetup-join/12%3ameeting_A1b2C3%40thread.v2/0?context=%7b%22Tid')
-
-
 def test_excluding_non_conference_urls():
-    event = get_event_from_file('non-meeting.json')
-    tc.assertEqual(event.conference_url, None)
-
-
-def test_location():
-    event = get_event_from_file('location.json')
-    tc.assertEqual(
-        event.conference_url,
-        'https://zoom.us/j/123456?pwd=AiBjCk')
+    event = get_event_with_defaults(location='https://github.com')
+    case.assertEqual(event.conference_url, None)
