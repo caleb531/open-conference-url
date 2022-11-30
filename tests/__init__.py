7d161989b2d9326b615648be3aadd7e5775f808c
Caleb Evans <caleb@calebevans.me>
2022-11-30 12:23:06 -0800

    Add Dialpad to tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@
 import os
 
 
-os.environ['conference_domains'] = '*.zoom.us, zoom.us, meet.google.com, hangouts.google.com, *.microsoft.com, gotomeeting.com, *.gotomeeting.com, webex.com, *.webex.com, uberconference.com'  # noqa
+os.environ['conference_domains'] = '*.zoom.us, zoom.us, meet.google.com, hangouts.google.com, *.microsoft.com, gotomeeting.com, *.gotomeeting.com, webex.com, *.webex.com, *.dialpad.com, uberconference.com'  # noqa
 os.environ['calendar_names'] = ''
 os.environ['use_direct_zoom'] = 'false'
 os.environ['use_direct_msteams'] = 'false'
