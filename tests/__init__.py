4ae9efbecbd1dd500d6617503ba984d0ee976b19
Caleb Evans <caleb@calebevans.me>
2022-11-30 12:46:29 -0800

    Use python-dotenv to load in test environment variables

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-import os
+from dotenv import load_dotenv
 
-
-os.environ['conference_domains'] = '*.zoom.us, zoom.us, meet.google.com, hangouts.google.com, *.microsoft.com, meet.goto.com, *.webex.com, webex.com, meetings.dialpad.com'  # noqa
-os.environ['calendar_names'] = ''
-os.environ['use_direct_zoom'] = 'false'
-os.environ['use_direct_msteams'] = 'false'
-os.environ['event_time_threshold_mins'] = '20'
+load_dotenv('tests/.env.test')
