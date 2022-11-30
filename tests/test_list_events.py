9b4fd60614931dacbc7b8112d563383434f54a4e
Caleb Evans <caleb@calebevans.me>
2022-11-29 20:57:50 -0800

    Add test for all-day meetings alongside upcoming meetings

--- tests/test_list_events.py
+++ tests/test_list_events.py
@@ -87,13 +87,40 @@ def test_after_window(out, get_events_today):
 }])
 @freeze_time('2022-10-16 8:00:00')
 @redirect_stdout
-def test_all_day(out, get_events_today):
-    """Should list all-day meetings as part of list of today's events"""
+def test_all_day_standalone(out, get_events_today):
+    """Should list all-day meetings by themselves"""
     list_events.main()
     feedback = json.loads(out.getvalue())
     case.assertEqual(feedback['items'][0]['title'], 'All-Day Conference')
     case.assertEqual(feedback['items'][0]['subtitle'], 'All-Day')
     case.assertEqual(len(feedback['items']), 1)
+
+
+@use_event_blobs([
+    {
+        'title': 'All-Day Conference',
+        'startDate': '2022-10-16T00:00',
+        'isAllDay': 'true',
+        'notes': 'https://zoom.us/j/123456'
+    },
+    {
+        'title': 'Morning Scrum',
+        'startDate': '2022-10-16T08:00',
+        'endDate': '2022-10-16T09:00',
+        'notes': 'https://zoom.us/j/123456'
+    }
+])
+@freeze_time('2022-10-16 7:58:00')
+@redirect_stdout
+def test_all_day_mixed(out, get_events_today):
+    """Should list all-day meetings alongside upcoming meetings"""
+    list_events.main()
+    feedback = json.loads(out.getvalue())
+    case.assertEqual(feedback['items'][0]['title'], 'All-Day Conference')
+    case.assertEqual(feedback['items'][0]['subtitle'], 'All-Day')
+    case.assertEqual(feedback['items'][1]['title'], 'Morning Scrum')
+    case.assertEqual(feedback['items'][1]['subtitle'], '8:00am')
+    case.assertEqual(len(feedback['items']), 2)
 
 
 @use_event_blobs([])
