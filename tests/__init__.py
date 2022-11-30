f70a6a42be0b95b2696b0a09f0c8200774dd868e
Caleb Evans <caleb@calebevans.me>
2022-11-30 12:38:10 -0800

    Remove UberConference from domains list entirely
    Since the rebrand to Dialpad, I doubt we'll see any new meeting links
    for uberconference.com.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@
 import os
 
 
-os.environ['conference_domains'] = '*.zoom.us, zoom.us, meet.google.com, hangouts.google.com, *.microsoft.com, meet.goto.com, *.webex.com, webex.com, meetings.dialpad.com, uberconference.com'  # noqa
+os.environ['conference_domains'] = '*.zoom.us, zoom.us, meet.google.com, hangouts.google.com, *.microsoft.com, meet.goto.com, *.webex.com, webex.com, meetings.dialpad.com'  # noqa
 os.environ['calendar_names'] = ''
 os.environ['use_direct_zoom'] = 'false'
 os.environ['use_direct_msteams'] = 'false'
