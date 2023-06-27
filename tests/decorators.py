a15a99707786b50a289d60e677e141875374f691
Caleb Evans <caleb@calebevans.me>
2023-06-26 19:31:02 -0700

    Write decorator that works for both functions and generators
    This solves the real problem that 342b790 (unsuccessfully) tried to
    solve. That commit actually introduced a problem where test generators
    were not running at all. This was recently indicated by a drop in
    coverage which inspired me to look into this further and produce this
    commit.

--- tests/decorators.py
+++ tests/decorators.py
@@ -2,6 +2,7 @@
 
 import os
 import os.path
+import inspect
 import json
 import sys
 from functools import wraps
@@ -23,23 +24,34 @@ def redirect_stdout(func):
     return wrapper
 
 
-# Derived from: <https://gist.github.com/LeoHuckvale/8f50f8f2a6235512827b>
+# Derived from: <https://gist.github.com/LeoHuckvale/8f50f8f2a6235512827b> and
+# <https://stackoverflow.com/questions/64622473/can-you-write-a-python-decorator-that-works-for-generator-functions-and-normal-f>
 def use_env(key, value):
     """
-    Sets an environment variable for only the lifetime of the given function
+    Sets an environment variable for only the lifetime of the given function;
+    this decorator works for both functions and generators
     """
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Setting only a single environment variable at a time ensures that
-            # this decorator can be used multiple times on the same function
+        def function_wrapper(*args, **kwargs):
             orig_value = os.environ.get(key, '')
             os.environ[key] = value
             try:
-                return func(*args, **kwargs)
+                return_value = func(*args, **kwargs)
             finally:
                 os.environ[key] = orig_value
-        return wrapper
+            return return_value
+
+        @wraps(func)
+        def generator_wrapper(*args, **kwargs):
+            orig_value = os.environ.get(key, '')
+            os.environ[key] = value
+            try:
+                return_value = yield from func(*args, **kwargs)
+            finally:
+                os.environ[key] = orig_value
+            return return_value
+        return generator_wrapper if inspect.isgeneratorfunction(func) else function_wrapper
 
     return decorator
 
