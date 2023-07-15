7243f5500424d3fb6bdbdd3dd5e614ea7eb99c5b
Caleb Evans <caleb@calebevans.me>
2023-07-15 09:58:16 -0700

    Add tests for BaseCalendar class

---
+++ tests/test_base_calendar.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import Mock
+
+from ocu.calendars.base_calendar import BaseCalendar
+
+
+case = unittest.TestCase()
+
+
+def test_base_calendar_not_instantiable():
+    """Should not be able to instantiate a BaseCalendar object by itself"""
+    with case.assertRaises(TypeError):
+        BaseCalendar()
+
+
+def test_get_event_dicts_not_implemented():
+    """
+    Should require the BaseCalendar.get_event_dicts() method to be implemented
+    by any subclass of BaseCalendar
+    """
+    with case.assertRaises(NotImplementedError):
+        BaseCalendar.get_event_dicts(Mock())
