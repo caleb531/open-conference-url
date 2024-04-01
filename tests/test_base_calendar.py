a73185cf8bede3188ea91aba1cada69a8a42e94d
Caleb Evans <caleb@calebevans.me>
2024-04-01 15:42:16 -0700

    Refactor base calendar tests to be class-based

--- tests/test_base_calendar.py
+++ tests/test_base_calendar.py
@@ -5,19 +5,18 @@ from unittest.mock import Mock
 
 from ocu.calendars.base_calendar import BaseCalendar
 
-case = unittest.TestCase()
 
+class TestBaseCalendar(unittest.TestCase):
 
-def test_base_calendar_not_instantiable():
-    """Should not be able to instantiate a BaseCalendar object by itself"""
-    with case.assertRaises(TypeError):
-        BaseCalendar()  # type: ignore
+    def test_base_calendar_not_instantiable(self):
+        """Should not be able to instantiate a BaseCalendar object by itself"""
+        with self.assertRaises(TypeError):
+            BaseCalendar()  # type: ignore
 
-
-def test_get_event_dicts_not_implemented():
-    """
-    Should require the BaseCalendar.get_event_dicts() method to be implemented
-    by any subclass of BaseCalendar
-    """
-    with case.assertRaises(NotImplementedError):
-        BaseCalendar.get_event_dicts(Mock())
+    def test_get_event_dicts_not_implemented(self):
+        """
+        Should require the BaseCalendar.get_event_dicts() method to be implemented
+        by any subclass of BaseCalendar
+        """
+        with self.assertRaises(NotImplementedError):
+            BaseCalendar.get_event_dicts(Mock())
